3dcb073d0377a914f9f222fb3fd9a95c2e0a5023
praveenreddy82472 <tumatipraveenreddy23@gmail.com>
2024-05-28 17:02:53 -0500

    files added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Praveen',
+    author_email= 'tumatipraveenreddy23@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
